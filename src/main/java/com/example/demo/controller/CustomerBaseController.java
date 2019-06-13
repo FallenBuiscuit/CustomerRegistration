e5fa0be609d27cc020b0951b393fcfb9b35c2862
carlo.jose.n.ruste <carlo.jose.n.ruste@accenture.com>
2019-06-13 15:07:09 +0800

    Development: Test Jenkins Build

--- src/main/java/com/example/demo/controller/CustomerBaseController.java
+++ src/main/java/com/example/demo/controller/CustomerBaseController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+/**
+ * Customer Base Controller
+ */
 public class CustomerBaseController {
 
     public static final String BASE_URL = "/api/v1/customers";
